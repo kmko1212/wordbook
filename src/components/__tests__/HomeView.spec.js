3298380c0b4d763f6631a340a067dd8f1beffcaf
kmko1212 <142073575+kmko1212@users.noreply.github.com>
2024-06-01 17:52:24 +0900

    add:HomeViewのテストを追加

--- src/components/__tests__/HomeView.spec.js
+++ src/components/__tests__/HomeView.spec.js
@@ -1,11 +1,60 @@
-import { describe, it, expect } from 'vitest'
+import { createRouter, createMemoryHistory } from 'vue-router'
+import { describe, it, expect, beforeEach } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import HomeView from '../../views/HomeView.vue'
 
+import ResisterView from '../../views/RegisterView.vue'
+import ConfirmView from '../../views/ConfirmView.vue'
+
+let router
+
+beforeEach(() => {
+  router = createRouter({
+    history: createMemoryHistory(),
+    routes: [
+      {
+        path: '/',
+        name: 'home',
+        component: HomeView
+      },
+      {
+        path: '/resister',
+        name: 'resister',
+        component: ResisterView
+      },
+      {
+        path: '/confirm',
+        name: 'confirm',
+        component: ConfirmView
+      }
+    ]
+  })
+})
+
 describe('HomeView', () => {
   it('renders properly', () => {
-    const wrapper = mount(HomeView)
+    const wrapper = mount(HomeView, { global: { plugins: [router] } })
     expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('navigates to /resister when #button-resister is clicked', async () => {
+    const wrapper = mount(HomeView, { global: { plugins: [router] } })
+
+    await wrapper.find('#button-resister').trigger('click')
+
+    await router.isReady()
+
+    expect(router.currentRoute.value.path).toBe('/resister')
+  })
+
+  it('navigates to /confirm when #button-confirm is clicked', async () => {
+    const wrapper = mount(HomeView, { global: { plugins: [router] } })
+
+    await wrapper.find('#button-confirm').trigger('click')
+
+    await router.isReady()
+
+    expect(router.currentRoute.value.path).toBe('/confirm')
   })
 })
